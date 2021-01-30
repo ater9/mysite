0310df972c4bfdb576af06099613f449d0b35cad
Ater <ater@outlook.jp>
2021-01-30 23:35:18 +0900

    To create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Template</title>
+        <link rel="stylesheet" href="tmp.css">
+    </head>
+    <body>
+        <?php
+        $hello = "Hello world.";
+        echo "<p>{$hello}</p>"; 
+        ?>
+    </body>
+</html>
